3f3f90d27b4a8c376f30e1e89ccad5083f790acf
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-05 22:15:16 +0530

    difference between frames

--- motion_detector.py
+++ motion_detector.py
@@ -1,4 +1,8 @@
 import cv2,time
+
+#regarded as the first static frame
+first_frame=None
+
 #capturing video through webcam
 video=cv2.VideoCapture(0)
 
@@ -6,8 +10,20 @@ while True:
     check, frame=video.read()
 
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Capturing",gray)
+    #removing noise
+    gray=cv2.GaussianBlur(gray,(21,21),0)
 
+    if first_frame is None:
+        first_frame=gray
+        continue
+    
+    #comparing the first frame with the current frame
+    delta_frame=cv2.absdiff(first_frame,gray)
+
+
+
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta frame",delta_frame)
     key=cv2.waitKey(1)
 
     if key==ord('q'):
