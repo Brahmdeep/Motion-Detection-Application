32a3376a714693b51895a4d40d9cefc7d5c58670
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-05 22:42:13 +0530

    checking for movement

--- motion_detector.py
+++ motion_detector.py
@@ -1,14 +1,16 @@
 import cv2,time
 
+
 #regarded as the first static frame
 first_frame=None
+status_list=[]
 
 #capturing video through webcam
 video=cv2.VideoCapture(0)
 
 while True:
     check, frame=video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     #removing noise
     gray=cv2.GaussianBlur(gray,(21,21),0)
@@ -25,15 +27,18 @@ while True:
     (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour)<1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status=1
         #creating a rectangle
         (x,y,w,h)=cv2.boundingRect(contour)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-
+    
+    status_list.append(status)
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta frame",delta_frame)
     cv2.imshow("Threshold frame",thresh_frame)
+    cv2.imshow("Color Frame",frame)
 
     key=cv2.waitKey(1)
 
